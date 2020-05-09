98520af87d06f9902bbddc86094d821fc51e9d8e
Sebastian Colomar <sebastian.colomar@gmail.com>
2020-05-09 10:35:56 +0200

    Add script to deploy Kubernetes repo

---
+++ Shell/deploy-repo.sh
@@ -0,0 +1,30 @@
+###############################################################################
+#        Copyright (C) 2020        Sebastian Francisco Colomar Bauza          #
+#        SPDX-License-Identifier:  GPL-2.0-only                               #
+###############################################################################
+
+#!/bin/bash
+
+set +x && test "$debug" = true && set -x ;
+
+pwd=$( dirname $( readlink -f $0 ) ) ;
+
+test -z "$stack" && echo PLEASE DEFINE THE VALUE FOR stack && exit 1 ;
+
+git clone https://github.com/secobau/docker.git ;
+source docker/AWS/common/functions.sh ;
+rm --recursive --force docker ;
+
+repo=https://raw.githubusercontent.com/secobau/proxy2aws/master/etc/yum.repos.d/kubernetes.repo;
+
+command="wget $repo && sudo mv $repo /etc/yum.repos.d/"
+targets=" InstanceManager1 " ;
+for target in $targets ; do
+ send_command "$command" "$target" "$stack" ;
+done ;
+
+command="ls -l /etc/yum.repos.d/$repo " ;
+targets=" InstanceManager1 " ;
+for target in $targets ; do
+ send_list_command "$command" "$target" "$stack" ;
+done ;
