ff810b05c99fe47961d01717437717e6e9fd545f
Sebastian Colomar <sebastian.colomar@gmail.com>
2020-05-13 06:33:18 +0200

    Add support for A/B deployments

--- Kubernetes/Shell/deploy.sh
+++ Kubernetes/Shell/deploy.sh
@@ -12,6 +12,8 @@ pwd=$( dirname $( readlink -f $0 ) ) ;
 test -z "$stack" && echo PLEASE DEFINE THE VALUE FOR stack && exit 1 ;
 test -z "$deploy" && echo PLEASE DEFINE THE TYPE OF DEPLOYMENT: LATEST/RELEASE && exit 1 ; 
 
+kubeconfig=/etc/kubernetes/admin.conf;
+
 git clone https://github.com/secobau/docker.git ;
 source docker/AWS/common/functions.sh ;
 rm --recursive --force docker ;
@@ -19,66 +21,59 @@ rm --recursive --force docker ;
 command=" git clone https://github.com/secobau/proxy2aws.git proxy2aws " ;
 targets=" InstanceManager1 " ;
 for target in $targets ; do
- send_command "$command" "$target" "$stack" ;
+  send_command "$command" "$target" "$stack" ;
 done ;
 
 command=" find proxy2aws " ;
 targets=" InstanceManager1 " ;
 for target in $targets ; do
- send_command "$command" "$target" "$stack" ;
+  send_command "$command" "$target" "$stack" ;
 done ;
 
 folder=/configs/etc/nginx/conf.d;
 file=aws2cloud.conf;
-command=" sudo kubectl create configmap $file --from-file $folder/$file --kubeconfig /etc/kubernetes/admin.conf "
+command=" sudo kubectl create configmap $file --from-file $folder/$file --kubeconfig $kubeconfig ";
 targets=" InstanceManager1 " ;
 for target in $targets ; do
- send_list_command "$command" "$target" "$stack" ;
+  send_list_command "$command" "$target" "$stack" ;
 done ;
 
 folder=/secrets/etc/nginx;
 file=aws2cloud.htpasswd;
-command=" sudo kubectl create secret generic $file --from-file $folder/$file --kubeconfig /etc/kubernetes/admin.conf "
+command=" sudo kubectl create secret generic $file --from-file $folder/$file --kubeconfig $kubeconfig ";
 targets=" InstanceManager1 " ;
 for target in $targets ; do
- send_list_command "$command" "$target" "$stack" ;
-done ;
-
-folder=/secrets/etc/nginx;
-file=aws2cloud.htpasswd;
-command=" sudo kubectl create secret generic $file --from-file $folder/$file --kubeconfig /etc/kubernetes/admin.conf "
-targets=" InstanceManager1 " ;
-for target in $targets ; do
- send_list_command "$command" "$target" "$stack" ;
+  send_list_command "$command" "$target" "$stack" ;
 done ;
 
 folder=/secrets/etc/nginx/conf.d;
 file=cloud2instance.conf;
-command=" sudo kubectl create secret generic $file --from-file $folder/$file --kubeconfig /etc/kubernetes/admin.conf ";
+command=" sudo kubectl create secret generic $file --from-file $folder/$file --kubeconfig $kubeconfig ";
 targets=" InstanceManager1 " ;
 for target in $targets ; do
- send_command "$command" "$target" "$stack" ;
+  send_command "$command" "$target" "$stack" ;
 done ;
 
 folder=/secrets/etc/nginx/conf.d;
 file=aws2prem.conf;
-command=" sudo kubectl create secret generic $file --from-file $folder/$file --kubeconfig /etc/kubernetes/admin.conf ";
+command=" sudo kubectl create secret generic $file --from-file $folder/$file --kubeconfig $kubeconfig ";
 targets=" InstanceManager1 " ;
 for target in $targets ; do
- send_command "$command" "$target" "$stack" ;
+  send_command "$command" "$target" "$stack" ;
 done ;
 
-command=" sudo kubectl apply --filename proxy2aws/Kubernetes/$deploy/aws2cloud.yaml --kubeconfig /etc/kubernetes/admin.conf ";
-targets=" InstanceManager1 " ;
-for target in $targets ; do
- send_list_command "$command" "$target" "$stack" ;
-done ;
-
-command=" sudo kubectl apply --filename proxy2aws/Kubernetes/$deploy/aws2prem.yaml --kubeconfig /etc/kubernetes/admin.conf ";
-targets=" InstanceManager1 " ;
-for target in $targets ; do
- send_list_command "$command" "$target" "$stack" ;
-done ;
+apps=" aws2cloud-BLUE aws2prem-BLUE ";
+apps=" aws2cloud aws2prem ";
+folder=proxy2aws/Kubernetes/$deploy;
+for app in $apps;
+do
+  command=" sudo kubectl apply --filename $folder/$app.yaml --kubeconfig $kubeconfig ";
+  targets=" InstanceManager1 ";
+  for target in $targets;
+  do
+    send_list_command "$command" "$target" "$stack";
+  done;
+done;
 
 command=" sudo rm --recursive --force proxy2aws " ;
 targets=" InstanceManager1 " ;
