0109e622cebbeb064cd93da5f7a7e1647030b69c
Sebastian Colomar <sebastian.colomar@gmail.com>
2020-05-15 10:33:40 +0200

    Add SSM Wrapper

--- Kubernetes/Shell/remove-ssm.sh
+++ Kubernetes/Shell/remove-ssm.sh
@@ -3,13 +3,11 @@
 #      Copyright (C) 2020        Sebastian Francisco Colomar Bauza      #
 #      SPDX-License-Identifier:  GPL-2.0-only                           #
 #########################################################################
+set +x && test "$debug" = true && set -x 				;
+#########################################################################
 export debug=$debug							;
 export deploy=$deploy							;
 export stack=$stack							;
-#########################################################################
-set +x && test "$debug" = true && set -x 				;
-#########################################################################
-pwd=$( dirname $( readlink -f $0 ) ) 					;
 #########################################################################
 domain=raw.githubusercontent.com                                        ;
 #########################################################################
@@ -23,5 +21,5 @@ cd $pwd && rm --recursive --force $path                                 ;
 file=remove.sh    	                                                ;
 path=secobau/proxy2aws/master/Kubernetes/Shell				;
 targets=" InstanceManager1 " 						;
-exec_remote_file_targets $domain "$export" $file $path $stack "$targets"		;
+exec_remote_file_targets $domain "$export" $file $path $stack "$targets";
 #########################################################################
