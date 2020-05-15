4eb8664acb67e5d014726f7fbbc5fda432a304bb
Sebastian Colomar <sebastian.colomar@gmail.com>
2020-05-15 08:40:57 +0200

    Add SSM Wrapper

--- Swarm/Shell/deploy.sh
+++ Swarm/Shell/deploy.sh
@@ -3,18 +3,18 @@
 #      Copyright (C) 2020        Sebastian Francisco Colomar Bauza      #
 #      SPDX-License-Identifier:  GPL-2.0-only                           #
 #########################################################################
+set +x && test "$debug" = true && set -x 				;
+#########################################################################
 export debug=$debug							;
 export deploy=$deploy							;
-#########################################################################
-set +x && test "$debug" = true && set -x 				;
 #########################################################################
 apps=" aws2cloud aws2prem aws2cloud-BLUE aws2prem-BLUE "		;
 domain=raw.githubusercontent.com                                        ;
 path=secobau/proxy2aws/master/Swarm/$deploy				;
-#########################################################################
 for app in $apps							;
 do 									\
   curl -O https://$domain/$path/$app.yaml                               ;
   sudo docker stack deploy --compose-file $app.yaml $app 		;
+  rm --force $app.yaml							;
 done									;
 #########################################################################
