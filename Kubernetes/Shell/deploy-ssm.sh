ad1a08a971e38730daa42e95324820d058226ceb
Sebastian Colomar <sebastian.colomar@gmail.com>
2020-05-15 10:31:23 +0200

    Add SSM Wrapper

--- Kubernetes/Shell/deploy-ssm.sh
+++ Kubernetes/Shell/deploy-ssm.sh
@@ -3,11 +3,11 @@
 #      Copyright (C) 2020        Sebastian Francisco Colomar Bauza      #
 #      SPDX-License-Identifier:  GPL-2.0-only                           #
 #########################################################################
+set +x && test "$debug" = true && set -x 				;
+#########################################################################
 export debug=$debug							;
 export deploy=$deploy							;
 export stack=$stack							;
-#########################################################################
-set +x && test "$debug" = true && set -x 				;
 #########################################################################
 domain=raw.githubusercontent.com                                        ;
 #########################################################################
@@ -21,5 +21,5 @@ cd $pwd && rm --recursive --force $path                                 ;
 file=deploy.sh    	                                                ;
 path=secobau/proxy2aws/master/Kubernetes/Shell				;
 targets=" InstanceManager1 " 						;
-exec_remote_file_targets $domain "$export" $file $path $stack "$targets"		;
+exec_remote_file_targets $domain "$export" $file $path $stack "$targets";
 #########################################################################
