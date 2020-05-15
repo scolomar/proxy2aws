d4d9ad8862fd9bf19205aae3f081d53e0a99d860
Sebastian Colomar <sebastian.colomar@gmail.com>
2020-05-16 01:09:26 +0200

    Add SSM Wrapper

--- Shell/deploy.sh
+++ Shell/deploy.sh
@@ -5,6 +5,17 @@
 #########################################################################
 set +x && test "$debug" = true && set -x				;
 #########################################################################
+test -n "$debug" || exit 100						;
+test -n "$deploy" || exit 100						;
+test -n "$HostedZoneName" || exit 100					;
+test -n "$Identifier" || exit 100					;
+test -n "$mode" || exit 100						;
+test -n "$RecordSetName1" || exit 100					;
+test -n "$RecordSetName2" || exit 100					;
+test -n "$RecordSetName3" || exit 100					;
+test -n "$s3domain" || exit 100						;
+test -n "$stack" || exit 100						;
+#########################################################################
 export debug=$debug							;
 export deploy=$deploy							;
 export HostedZoneName=$HostedZoneName					;
@@ -16,10 +27,15 @@ export RecordSetName3=$RecordSetName3					;
 export s3domain=$s3domain						;
 export stack=$stack							;
 #########################################################################
+export -f exec_remote_file						;
+export -f send_list_command						;
+export -f send_remote_file						;
+#########################################################################
 domain=raw.githubusercontent.com                                        ;
 #########################################################################
 file=functions.sh                                                       ;
 path=secobau/docker/master/AWS/common                                   ;
+#########################################################################
 pwd=$PWD && mkdir --parents $path && cd $path                           ;
 curl -O https://$domain/$path/$file                                     ;
 source ./$file                                                          ;
@@ -27,24 +43,26 @@ cd $pwd && rm --recursive --force $path                                 ;
 #########################################################################
 file=deploy.sh                                               		;
 path=secobau/docker/master/AWS/install/AMI				;
+#########################################################################
 exec_remote_file $domain $file $path				 	;
 #########################################################################
-export -f exec_remote_file						;
-export -f send_remote_file						;
-export -f send_list_command						;
 file=cluster.sh                                               		;
+#########################################################################
 path=secobau/docker/master/AWS/install/$mode				;
 exec_remote_file $domain $file $path				 	;
 #########################################################################
 file=deploy-config-ssm.sh                                               ;
 path=secobau/proxy2aws/master/Shell                                     ;
+#########################################################################
 exec_remote_file $domain $file $path				 	;
 #########################################################################
 file=deploy-ssm.sh      	                                        ;
 path=secobau/proxy2aws/master/Shell                               	;
+#########################################################################
 exec_remote_file $domain $file $path				 	;
 #########################################################################
 file=remove-config-ssm.sh                                               ;
 path=secobau/proxy2aws/master/Shell                                     ;
+#########################################################################
 exec_remote_file $domain $file $path				 	;
 #########################################################################
