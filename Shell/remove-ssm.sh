6ce84d2c06661283e269d2f9c7bcacf76b422710
Sebastian Colomar <sebastian.colomar@gmail.com>
2020-05-15 12:11:37 +0200

    Add SSM Wrapper

---
+++ Shell/remove-ssm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+#########################################################################
+#      Copyright (C) 2020        Sebastian Francisco Colomar Bauza      #
+#      SPDX-License-Identifier:  GPL-2.0-only                           #
+#########################################################################
+set +x && test "$debug" = true && set -x 				;
+#########################################################################
+domain=raw.githubusercontent.com                                        ;
+export=" export debug=$debug "						;
+file=remove.sh    	                                                ;
+path=secobau/proxy2aws/master/$mode/Shell				;
+targets=" InstanceManager1 " 						;
+#########################################################################
+export=" $export 							\
+  &&									\
+  export deploy=$deploy							\
+"									;
+send_remote_file $domain "$export" $file $path $stack "$targets"	;
+#########################################################################
