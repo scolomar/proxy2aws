f02e1fbe1c9e17b8de6a959b72118e02e31300a7
Sebastian Colomar <sebastian.colomar@gmail.com>
2020-05-15 08:47:03 +0200

    Add SSM Wrapper

--- Shell/remove-config.sh
+++ Shell/remove-config.sh
@@ -3,9 +3,9 @@
 #      Copyright (C) 2020        Sebastian Francisco Colomar Bauza      #
 #      SPDX-License-Identifier:  GPL-2.0-only                           #
 #########################################################################
-export debug=$debug							;
-#########################################################################
 set +x && test "$debug" = true && set -x 				;
+#########################################################################
+export debug=$debug							;
 #########################################################################
 folders=" configs secrets " 						;
 for folder in $folders 							;
